5f1f98da8a9fba9beb2d8601c6089d6d9c02cc79
carolinechaim <caroline.chaim@gmail.com>
2021-08-03 09:46:16 -0300

    Implementacao do template da pagina principal em dois containers teste. Mudanca de next para react normal e aderencia do yarn na construcao do projeto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Teste from "./pages/teste/teste";
+import Teeste from './pages/teste2/Teeste';
+
+
+function App() {
+  return (
+    <div>
+      <Teste/>
+      <Teeste />
+    </div>
+  );
+}
+
+export default App;
